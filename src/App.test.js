8f348ccaee76f6f703ddccd2abdf9feae7dd5cea
Christopher Ashton <christopher.ashton@madetech.com>
2023-01-11 14:14:18 +0000

    add next test, create chatbot form

--- src/App.test.js
+++ src/App.test.js
@@ -1,8 +1,20 @@
 import { render, screen } from '@testing-library/react';
 import App from './App';
+import ChatBot from "./Components/ChatBot";
 
-test('renders learn react link', () => {
-  render(<App />);
-  const linkElement = screen.getByText(/learn react/i);
-  expect(linkElement).toBeInTheDocument();
+
+describe("Tests for the header component", () =>{
+  test('renders learn react link', () => {
+    render(<App />);
+    const linkElement = screen.getByText(/Apprentee Chatbot Helper/i);
+    expect(linkElement).toBeInTheDocument();
+  });
+})
+
+describe("Tests for the chat bot component", () => {
+  test('should have a textbox on the screen for the user to use', () => {
+    render(<ChatBot />);
+    const linkElement = screen.getByRole("textbox");
+    expect(linkElement).toBeInTheDocument();
+  });
 });
--- src/Components/ChatBot.jsx
+++ src/Components/ChatBot.jsx
@@ -1,36 +1,52 @@
-import React, { useState } from 'react';
-import openai from 'openai';
-
-function ChatGPT() {
-  const [input, setInput] = useState('');
-  const [response, setResponse] = useState('');
-
-  const handleInputChange = event => {
-    setInput(event.target.value);
-  };
-
-  const handleSubmit = async event => {
-    event.preventDefault();
-    openai.apiKey = "YOUR_API_KEY_HERE";
-    const responseData = await openai.Completion.create({
-      prompt: `You: ${input}\nBot:`,
-      model: 'text-davinci-002',
-      max_tokens: 100,
-    });
-    setResponse(responseData.choices[0].text);
-  };
-
+export default function ChatBot() {
   return (
-    <div>
-      <form onSubmit={handleSubmit}>
-        <input type="text" value={input} onChange={handleInputChange} />
+     <form>
+        <input type="text" value="input" />
         <button type="submit">Submit</button>
-      </form>
-      <div>
-        {response}
-      </div>
-    </div>
-  );
+     </form>  )
 }
 
-export default ChatGPT;
+// <form>
+//   <input type="text" value="input" />
+//   <button type="submit">Submit</button>
+// </form>
+
+
+
+
+// import React, { useState } from 'react';
+// import openai from 'openai';
+//
+// function ChatGPT() {
+//   const [input, setInput] = useState('');
+//   const [response, setResponse] = useState('');
+//
+//   const handleInputChange = event => {
+//     setInput(event.target.value);
+//   };
+//
+//   const handleSubmit = async event => {
+//     event.preventDefault();
+//     openai.apiKey = "YOUR_API_KEY_HERE";
+//     const responseData = await openai.Completion.create({
+//       prompt: `You: ${input}\nBot:`,
+//       model: 'text-davinci-002',
+//       max_tokens: 100,
+//     });
+//     setResponse(responseData.choices[0].text);
+//   };
+//
+//   return (
+//     <div>
+//       <form onSubmit={handleSubmit}>
+//         <input type="text" value={input} onChange={handleInputChange} />
+//         <button type="submit">Submit</button>
+//       </form>
+//       <div>
+//         {response}
+//       </div>
+//     </div>
+//   );
+// }
+//
+// export default ChatGPT;
