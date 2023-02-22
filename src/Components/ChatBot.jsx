d22f5d8d8a94110526b2c19a21062e15dbfcb0d8
eddie ashton <eddie.ashton@madetech.com>
2023-02-22 16:38:09 +0000

    Add autoScroll and autoFocus :)

--- src/Components/ChatBot.jsx
+++ src/Components/ChatBot.jsx
@@ -17,7 +17,7 @@ export default function ChatBot() {
       console.log(messages);
       if (userLastMessage) {
           // Send me to the API!
-          const userMessage = messages.slice(-1);
+          //const userMessage = messages.slice(-1);
 
           setInputEnabled(false);
           setButtonEnabled(false);
@@ -67,7 +67,7 @@ export default function ChatBot() {
     const scrollToBottom = () => {
         messagesEndRef.current?.scrollIntoView({ behavior: "smooth" });
     };
-    useEffect(scrollToBottom, [messages]);
+    useEffect(scrollToBottom, [inputEnabled]);
 
   return (
       <>
@@ -85,77 +85,3 @@ export default function ChatBot() {
   )
 }
 
-
-
-
-
-
-
-// import React, { useState } from 'react';
-// import openai from 'react-openai-api';
-//
-// function ChatGPT() {
-//   const [input, setInput] = useState('');
-//   const [response, setResponse] = useState('');
-//
-//   const handleInputChange = event => {
-//     setInput(event.target.value);
-//   };
-//
-//   const handleSubmit = async event => {
-//     event.preventDefault();
-//     openai.apiKey = process.env.REACT_APP_API_KEY;
-//     const responseData = await openai.Completion.create({
-//       prompt: `You: ${input}\nBot:`,
-//       model: 'text-davinci-002',
-//       max_tokens: 100,
-//     });
-//     setResponse(responseData.choices[0].text);
-//   };
-//
-//   return (
-//     <div>
-//       <form onSubmit={handleSubmit}>
-//         <input type="text" value={input} onChange={handleInputChange} />
-//         <button type="submit">Submit</button>
-//       </form>
-//       <div>
-//         {response}
-//       </div>
-//     </div>
-//   );
-// }
-//
-// export default ChatGPT;
-
-// $("#terminal").terminal(async function (command, terminal) {
-//         try {
-//             const prompt = `you are a helpful, knowledge sharing chatbot. I say: ${command}. You reply:`
-//             const response = await fetch(
-//                 `https://api.openai.com/v1/completions`,
-//                 {
-//                     body: JSON.stringify({"model": "text-davinci-003", "prompt": prompt, "temperature": 0.86, "max_tokens": 256}),
-//                     method: "POST",
-//                     headers: {
-//                         "content-type": "application/json",
-//                         Authorization: "Bearer [YOUR API KEY]",
-//                     },
-//                 }
-//             ).then((response) => {
-//                 if (response.ok) {
-//                     response.json().then((json) => {
-//                         terminal.echo(json.choices[0].text.trim());
-//                     });
-//                 }
-//             });
-//
-//             console.log("Completed!");
-//         } catch (err) { console.error(`Error: ${err}`) }
-//     },
-//     {
-//         greetings: 'GPT-3 Chatbot v0.1',
-//         name: 'gpt3_demo',
-//         height: 400,
-//         width: 800,
-//         prompt: '> '
-//     });
--- src/Components/atoms/FormInput.jsx
+++ src/Components/atoms/FormInput.jsx
@@ -1,13 +1,19 @@
 import {FormControl} from "react-bootstrap";
+import {useEffect, useRef} from "react";
 
 export default function FormInput(props) {
 
+    const inputEl = useRef(null)
+
+    useEffect(() => {
+        inputEl.current.focus();
+    }, [props.inputEnabled]);
     function handleChange(e) {
         e.preventDefault();
         props.setInput(e.target.value)
     }
 
     return (
-        <FormControl type="text" placeholder={"Please Type!"} value={props.input} onChange={handleChange} disabled={!props.inputEnabled} />
+        <FormControl autoFocus="true" ref={inputEl} type="text" placeholder={"Please Type!"} value={props.input} onChange={handleChange} disabled={!props.inputEnabled} />
     )
 }
